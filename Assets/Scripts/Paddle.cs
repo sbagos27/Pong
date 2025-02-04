f533720e649f0be5f2c7615cd5427fbf8087ca12
sbagos27 <sbagos@csumb.edu>
2025-02-03 22:48:59 -0800

    Got the left paddle to work

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Paddle : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+    public float paddleSpeed;
+  
+    private Vector3 paddleDirection;
+    
+    public InputActionReference move;
+  
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(paddleDirection.x * paddleSpeed, paddleDirection.y * paddleSpeed);
+    }
+    
+    void Update()
+    {
+        paddleDirection = move.action.ReadValue<Vector3>();
+    }
+}
